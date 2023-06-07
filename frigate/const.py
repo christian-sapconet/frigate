f572a82e8a4e580e231757f967791efa1a634dab
Christian Olivier <christian.olivier@sapconet.co.za>
2023-06-07 17:50:39 +0200

    Working with Gotcha backend

--- frigate/const.py
+++ frigate/const.py
@@ -5,7 +5,7 @@ BIRDSEYE_PIPE = "/tmp/cache/birdseye"
 CACHE_DIR = "/tmp/cache"
 YAML_EXT = (".yaml", ".yml")
 PLUS_ENV_VAR = "PLUS_API_KEY"
-PLUS_API_HOST = "https://api.frigate.video"
+PLUS_API_HOST = "https://gotcha.bankey.co.za"
 MAX_SEGMENT_DURATION = 600
 BTBN_PATH = "/usr/lib/btbn-ffmpeg"
 
